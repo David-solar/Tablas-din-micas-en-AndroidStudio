690d3cf835f98eb6c5b078b6767330e11c164798
David-solar <davisolar94@gmail.com>
2016-05-15 16:58:44 -0500

    tablas dinamicas en AS

---
+++ Pruebadetablas/app/src/main/java/com/desmov/subneteo/pruebadetablas/MainActivity.java
@@ -0,0 +1,82 @@
+package com.desmov.subneteo.pruebadetablas;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TableLayout;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        //Tabla tabla = new Tabla(this, (TableLayout)findViewById(R.id.tabla));
+
+        TableLayout tt = (TableLayout) findViewById(R.id.tabla);
+
+        Tabla tab = new Tabla(this, tt);
+
+        //se le manda el array de la cabecera
+        tab.agregarCabecera(R.array.cabecera_tabla);
+
+
+        for(int i = 0; i < 15; i++)
+        {
+            ArrayList<String> elementos = new ArrayList<String>();
+            elementos.add(Integer.toString(i));
+            elementos.add("Casilla [" + i + ", 0]");
+            elementos.add("Casilla [" + i + ", 1]");
+            elementos.add("Casilla [" + i + ", 2]");
+            elementos.add("Casilla [" + i + ", 3]");
+            tab.agregarFilaTabla(elementos);
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
